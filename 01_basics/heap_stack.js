293b00aecab72b3f984421c41fed49bf1a0984dc
juned54 <mohammadjuned548@gmail.com>
2024-07-09 06:04:30 +0000

    confg syncronus

---
+++ 01_basics/heap_stack.js
@@ -0,0 +1,14 @@
+let myname="ahmad";
+let anothername=myname;
+anotheraname="juned";
+console.log(myname);
+console.log(anotheraname);
+let userOne = {
+    eamil:"juned@gmail.com",
+    password:"1234556"
+
+}
+let user2=userOne
+user2.email="ahmad@gmail.com";
+console.log(userOne);
+console.log( user2);
