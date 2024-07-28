da88cfa0632aca5cbc033354123c6eb5a91bfd86
juned54 <mohammadjuned548@gmail.com>
2024-07-28 08:05:12 +0000

    Container  config

---
+++ 01_basics/math_number.js
@@ -0,0 +1,13 @@
+let score = 400;
+console.log(score)
+const balance = new Number(100)
+console.log(balance)
+console .log(balance.toString().length); 
+console.log(balance.toFixed(1));
+const account=123.956;
+console.log(account.toPrecision(3));
+const hundered = 1000000;
+console.log(hundered.toLocaleString('en-In'));
+// +++++++++++++++++++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.round(4.6));
