eeef9d1a5fcefc6fa23129a827a01a0fb5a2af8f
juned54 <mohammadjuned548@gmail.com>
2024-07-28 12:41:04 +0000

    Container config

--- 01_basics/math_number.js
+++ 01_basics/math_number.js
@@ -11,3 +11,13 @@ console.log(hundered.toLocaleString('en-In'));
 // +++++++++++++++++++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++++++++++
 console.log(Math);
 console.log(Math.round(4.6));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.2));
+console.log(Math.min(4,2,8,7));
+console.log(Math.max(4,6,9,8,7));
+console.log((Math.random()*10)+1);
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+const min=10;
+const max=30;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
