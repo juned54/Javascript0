8bb6b59764b6139f63bfb39202860b91b683cba9
juned54 <mohammadjuned548@gmail.com>
2024-07-31 08:23:33 +0000

    Container config

---
+++ 01_basics/object.js
@@ -0,0 +1,17 @@
+const User= {}
+    User.name="junaid"
+    User.phone="911125970"
+   User. email="junnasheikh@gmail.com"
+
+
+ console.log(User.email)
+const name={
+    fullname:{
+        firtname:{
+            first:"juned",
+            surname:"sheikh",
+        }
+
+    }
+}
+console.log(name)
